efe69dffcd4226c879a719a8c50e0dfab6a3f225
cafemug <hyanghope@gmail.com>
2021-06-28 22:38:12 +0900

    sol: elimination Game

---
+++ solutions/EliminationGame.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/elimination-game/
+public class EliminationGame {
+    class Solution {
+        public int lastRemaining(int n) {
+            int start = 1;
+            int dir = 1;
+            int step = 1;
+            while(n>1){
+                if(dir == 1 || (dir == -1 && n%2 == 1)){
+                    start += step;
+                }
+                step *= 2;
+                dir *= -1;
+                n /= 2;
+            }
+            return start;
+        }
+    }
+}
